3342d09c2c80e7e5b4c95f309292a6799518c5aa
Sahu <kimo3998@gmail.com>
2024-11-01 23:04:28 -0400

    Open source fineurAI push

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='HeliosTuner',
+    version='1.0.3',
+    author='Errahum',
+    description='A project for fine-tuning OpenAI models using HeliosTuner.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Errahum/HeliosTuner-OpenAI-fine-tuning',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'pydantic',
+        'requests',
+        'python-dotenv',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'heliostuner=main_heliostuner:main_heliostuner',
+        ],
+    },
+    include_package_data=True,
+    license='MIT',
+)
